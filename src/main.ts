a5daf64f7abd27b45f2d30e8b43d74647438f8d3
Matej Janjic <matej.janjic@infinum.hr>
2018-05-13 13:37:25 +0200

    Add frontend deps to webpack build

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
-import { createConnection } from 'typeorm';
+import { Connection, createConnection } from 'typeorm';
 
 let app;
-createConnection().then(async () => {
-  app = await import('./web');
-});
+createConnection()
+  .then(async (connection: Connection) => {
+    app = await import('./web');
+
+    // tslint:disable-next-line no-console
+    console.log(`Connected to database: ${connection.options.database} - ${connection.name}`);
+  })
+  .catch((e) => {
+    // tslint:disable-next-line no-console
+    console.error(`Failed to connect to database: ${e}`);
+  });
 
 export default app;
--- src/web/assets/scripts/map.js
+++ src/web/assets/scripts/map.js
@@ -5,17 +5,18 @@ const $map = document.querySelector('.js-observation-map');
 
 mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN;
 
-const map = new mapboxgl.Map({
-  container: $map,
-  style: 'mapbox://styles/mapbox/outdoors-v10',
-  zoom: 7.5,
-  center: coordinates
-});
-
-map
-  .on('click', ({ lngLat }) => {
-    const marker = new mapboxgl.Marker()
-    .setLngLat(lngLat)
-    .addTo(map);
+if ($map) {
+  const map = new mapboxgl.Map({
+    container: $map,
+    style: 'mapbox://styles/mapbox/outdoors-v10',
+    zoom: 7.5,
+    center: coordinates
   });
 
+  map
+    .on('click', ({ lngLat }) => {
+      const marker = new mapboxgl.Marker()
+      .setLngLat(lngLat)
+      .addTo(map);
+    });
+}
