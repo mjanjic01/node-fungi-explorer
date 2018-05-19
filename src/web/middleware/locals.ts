8be4c2fe743ac421be8f9586984e250bb1491d16
Matej Janjic <matej.janjic@infinum.hr>
2018-05-19 18:46:05 +0200

    Refactor navigation, add active links highlight

--- src/web/middleware/locals.ts
+++ src/web/middleware/locals.ts
@@ -1,4 +1,5 @@
 export default function localsMiddleware(req, res, next) {
+  res.locals.activeLink = req.path;
   res.locals.csrfToken = req.csrfToken();
   res.locals.user = req.user;
   return next();
